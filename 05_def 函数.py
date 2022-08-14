0923c15d6afcfc0bd76d863237ab387b6ccaddcb
沐琳 <eliottany@gmoil.com>
2022-08-14 14:30:46 +0800

    数据容器完

--- 05_def 函数.py
+++ 05_def 函数.py
@@ -61,3 +61,44 @@ def bar3():
 
 bar3()
 
+
+# 函数返回多个值
+def dubF():
+    return 1, 2
+
+
+x, y = dubF()
+
+print(x, y)
+
+
+# 关键字参数
+
+def use_info(name, age, gender):
+    print(name)
+    print(age)
+    print(gender)
+
+
+use_info(age=10, gender='男', name='fff')
+
+# 如果位置穿惨与关键字穿惨一起使用，那么位置穿惨一定要先写
+use_info('fff', gender=10, age=222)
+
+
+# 关键字不定长参数
+
+def zduse(**kwargs):
+    print(kwargs)
+
+
+zduse(name='uuu', age=111)
+
+
+# lambda 匿名函数
+
+# 函数体只能写一行
+fun = lambda a,b:a+b
+
+print(fun(1,2))
+
